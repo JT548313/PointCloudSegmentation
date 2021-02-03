5c80e9e937d5d9fd91c87beef8009946e047d479
Jayati <jayati@jayati.lan>
2021-02-03 09:49:43 -0800

    First commit

---
+++ build/CMakeFiles/pcs_exe.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/jt/Projects/PointCloudSegmentation/PointCloudSegmentation.cpp" "/Users/jt/Projects/PointCloudSegmentation/build/CMakeFiles/pcs_exe.dir/PointCloudSegmentation.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DISABLE_LIBUSB_1_0"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "qh_QHpointer"
+  "vtkRenderingContext2D_AUTOINIT=1(vtkRenderingContextOpenGL2)"
+  "vtkRenderingCore_AUTOINIT=3(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingOpenGL2)"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/homebrew/Cellar/vtk@8.2/8.2.0_2/include/vtk-8.2"
+  "/opt/homebrew/include"
+  "/opt/homebrew/include/pcl-1.11"
+  "/opt/homebrew/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/pcs_exe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pcs_exe.dir/PointCloudSegmentation.cpp.o"
+  "pcs_exe"
+  "pcs_exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pcs_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
